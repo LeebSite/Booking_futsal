d6bfbbca36e30520438513240f3c5481818b5ba2
LeebSite <mhd.ghalibpradipa@gmail.com>
2024-12-05 00:21:43 +0700

    membuat view dan route booking lapangan (customer fe)

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 
+// Route untuk halaman landing
 Route::get('/', [LandingController::class, 'index']);
+
+// Route untuk halaman login
 Route::get('/masuk', function () {
     return view('login');
 });
+
+// Route untuk halaman pendaftaran
 Route::get('/daftar', function () {
     return view('register');
+});
+
+// Route untuk halaman booking lapangan (dalam folder customer)
+Route::get('/booking', function () {
+    return view('customer.bookinglap'); // Perhatikan tambahan 'customer.'
 });
