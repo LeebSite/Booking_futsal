493b246b2bade4df0f93ec8074518d0d2d7e61a0
LeebSite <mhd.ghalibpradipa@gmail.com>
2024-12-08 12:58:14 +0700

    fixing and update login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,27 +14,34 @@
                 Masuk ke Andi's Futsal
             </h2>
         </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
+        <form class="mt-8 space-y-6" action="{{ route('autentic') }}" method="POST">
+            @csrf
             <div class="rounded-md shadow-sm -space-y-px">
                 <div>
                     <label for="username" class="sr-only">Username</label>
-                    <input id="username" name="username" type="text" required class="appearance-none rounded-lg block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mb-4" placeholder="Masukkan username">
+                    <input id="username" name="username" type="text" value="{{ old('username') }}" required
+                        class="appearance-none rounded-lg block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm mb-4"
+                        placeholder="Masukkan username">
                 </div>
                 <div class="relative">
                     <label for="password" class="sr-only">Password</label>
-                    <input id="password" name="password" type="password" required class="appearance-none rounded-lg block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Masukkan password">
-                    <button type="button" id="togglePassword" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
-                        <i class="fas fa-eye text-gray-500"></i>
-                    </button>
+                    <input id="password" name="password" type="password" required
+                        class="appearance-none rounded-lg block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        placeholder="Masukkan password">
                 </div>
             </div>
-
+            @if ($errors->has('loginError'))
+            <p class="text-red-500 text-sm mt-2">{{ $errors->first('loginError') }}</p>
+            @endif
             <div>
-                <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <button type="submit"
+                    class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Masuk
                 </button>
             </div>
         </form>
+        
+        
         <div class="text-center">
             <p class="text-sm text-gray-600">
                 Belum memiliki akun? 
