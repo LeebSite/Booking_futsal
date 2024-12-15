f66bc95fbff1de8ba6516410183429c94cb04854
LeebSite <mhd.ghalibpradipa@gmail.com>
2024-12-15 17:00:01 +0700

    update migrations jadwal_lapangan dan pesanan

--- database/migrations/2024_12_06_231516_jadwal_lapangan.php
+++ database/migrations/2024_12_06_231516_jadwal_lapangan.php
@@ -10,14 +10,16 @@ return new class extends Migration
     {
         Schema::create('jadwal_lapangan', function (Blueprint $table) {
             $table->id('id');
-            $table->foreignId('id_lapangan')->constrained('lapangan', 'id'); // Tentukan foreign key
+            $table->foreignId('id_lapangan')->constrained('lapangan', 'id'); 
             $table->date('tanggal');
             $table->time('jam_mulai');
             $table->time('jam_selesai');
-            $table->enum('status', ['kosong', 'dipesan', 'terkonfirmasi'])->default('kosong');
+            $table->enum('status', ['kosong', 'dipesan'])->default('kosong');
             $table->timestamps();
-        });        
+        });
     }
+    
+
     public function down(): void
     {
         Schema::dropIfExists('jadwal_lapangan');
--- database/migrations/2024_12_06_231649_pesanan.php
+++ database/migrations/2024_12_06_231649_pesanan.php
@@ -8,17 +8,26 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('pesanan', function (Blueprint $table) {
-            $table->id('id'); // Primary key
-            $table->foreignId('id_pengguna')->constrained('pengguna', 'id'); // Foreign key ke tabel 'pengguna'
-            $table->foreignId('id_lapangan')->constrained('lapangan', 'id'); // Foreign key ke tabel 'lapangan'
-            $table->enum('status', ['menunggu_konfirmasi', 'diterima', 'ditolak'])->default('menunggu_konfirmasi');
-            $table->timestamps();
-        });        
-    }
+    Schema::create('pesanan', function (Blueprint $table) {
+        $table->id('id'); // Primary key
+        $table->foreignId('id_pengguna')->constrained('pengguna', 'id'); 
+        $table->foreignId('id_lapangan')->constrained('lapangan', 'id');
+        $table->date('tanggal');
+        $table->time('jam_mulai');
+        $table->time('jam_selesai');
+        $table->integer('jumlah_jam');
+        $table->decimal('total_biaya', 10, 2);
+        $table->string('nama_lengkap');
+        $table->text('alamat');
+        $table->string('no_telepon');
+        $table->enum('status', ['menunggu_konfirmasi', 'diterima', 'ditolak'])->default('menunggu_konfirmasi');
+        $table->timestamps();
+    });
+}
+
 
     public function down(): void
     {
-        Schema::dropIfExists('pemesanan');
+        Schema::dropIfExists('pesanan');
     }
 };
